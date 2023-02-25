1da3749c5df627f2876a67b282738dcd6609f567
Hera.r <tennyhera.r@gmail.com>
2023-02-25 22:04:30 +0100

    add ft_printf

--- pipex.c
+++ pipex.c
@@ -20,7 +20,7 @@ char	**get_path(char **envp)
 	return (tab);
 }
 
-int	access_execve(char **tab, char **args)
+char	*access_ok(char **tab, char **args)
 {
 	char	*tmp;
 	char	*path;
@@ -31,25 +31,72 @@ int	access_execve(char **tab, char **args)
 	{
 		tmp = ft_strjoin(tab[i], "/");
 		path = ft_strjoin(tmp, args[0]);
-
 		if (access(path, F_OK) != -1)
-		{
-			if (execve(path, args, NULL) == -1);
-				perror("execve");
-			break;
-		}
+			return (free(tmp), path);
 	i++;
+	free(tmp);
+	free(path);
 	}
-	return (-1);
+	return (0);
 }
 
 int	ft_chan(int fd_pipe, int fd)
 {
 	if (dup2(fd_pipe, STDIN_FILENO) < 0)
 		perror("Erreur de redirection de la sortie standard");
+	
 	if (dup2(fd, STDOUT_FILENO) < 0)
 		perror("Erreur de redirection de la sortie standard");
+
 	return (-1);
+}
+
+int	find_slash(char	*argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (argv[i] == '/')
+			return (1);
+	i++;
+	}
+	return (0);
+}
+
+char	*final_path(char *argv, char **envp)
+{
+	char	**tab;
+	char	**args;
+	char	*path;
+
+	tab = get_path(envp);
+	args = ft_split(argv, ' ');
+	path = access_ok(tab, args);
+	if (!path)
+		return (printf("command not found\n"), exit(EXIT_FAILURE), NULL);
+	return (path);
+}
+
+int	read_and_write(int fd1, int fd2)
+{
+	char buffer[1024];
+    ssize_t bytes_read;
+	ssize_t bytes_written;
+
+	bytes_read = read(fd1, buffer, sizeof(buffer));
+	buffer[bytes_read] = '\0';
+	if (bytes_read == -1) {
+        perror("Erreur lors de la lecture du fichier");
+        exit(EXIT_FAILURE);
+    }
+	bytes_written = write(fd2, buffer, ft_strlen(buffer));
+    if (bytes_written == -1) {
+        perror("Erreur lors de l'écriture dans le fichier");
+        exit(EXIT_FAILURE);
+    }
+	return (1);
 }
 
 int	main(int argc, char *argv[], char *envp[]) 
@@ -61,6 +108,7 @@ int	main(int argc, char *argv[], char *envp[])
 	pid_t		pid;
 	char		**args;
 	int 		status;
+	char		*path;
 	int			i;
 
 	i = 0;
@@ -70,8 +118,8 @@ int	main(int argc, char *argv[], char *envp[])
 		fd2 = open(argv[4], O_CREAT | O_WRONLY, 0644);
 		if (fd1 < 0 || fd2 < 0)
 			perror("Erreur d'ouverture du fichier");
-		args = ft_split(argv[2], ' ');
-
+		if (ft_strlen(argv[2]) == 0 && ft_strlen(argv[3]) == 0)
+			return (read_and_write(fd1, fd2));
 		if (pipe(fd_pipe) == -1)
 			return (perror("Erreur de création du canal de communication"), 1);
 
@@ -81,16 +129,28 @@ int	main(int argc, char *argv[], char *envp[])
 		else if (pid == 0)
 		{
 			close(fd_pipe[0]);
-			ft_chan(fd1, fd_pipe[1]);
-			tab = get_path(envp);
-			access_execve(tab, args);
+			if (ft_strlen(argv[2]) != 0)
+			{
+				path = final_path(argv[2], envp);
+				if (ft_strlen(argv[3]) == 0)
+					ft_chan(fd1, fd2);
+				else
+					ft_chan(fd1, fd_pipe[1]);
+				args = ft_split(argv[2], ' ');
+				if (execve(path, args, NULL) == -1);
+					perror("execve");
+			}
 			exit(EXIT_SUCCESS);
 		} else {
 			close(fd_pipe[1]);
-			ft_chan(fd_pipe[0], fd2);
-			tab = get_path(envp);
-			args = ft_split(argv[3], ' ');
-			access_execve(tab, args);
+			if (ft_strlen(argv[3]) != 0)
+			{
+				path = final_path(argv[3], envp);
+				ft_chan(fd_pipe[0], fd2);
+				args = ft_split(argv[3], ' ');
+				if (execve(path, args, NULL) == -1);
+					perror("execve");
+			}
 			wait(&status);
 		}
 		close(fd1);
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: hrandria <hrandria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 12:36:16 by hrandria          #+#    #+#             */
-/*   Updated: 2023/02/23 23:47:16 by hrandria         ###   ########.fr       */
+/*   Updated: 2023/02/25 16:55:17 by hrandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <unistd.h>
 # include <stdint.h>
 # include <stdio.h>
+# include <stdarg.h>
 #include <fcntl.h>
 #include <sys/types.h>
  #include <sys/wait.h>
@@ -25,6 +26,15 @@
 size_t	ft_strlen(const char *s);
 char	**ft_split(char const *s, char c);
 char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_printf(const char *fmt, ...);
+int		ft_putchar(char c);
+int		ft_putstr(char *s);
+void	ft_putnbr(int nb, int *result);
+void	ft_hexadecimal(unsigned int nb, int *result, char c);
+void	ft_nbunsigned(unsigned int nb, int *result);
+void	ft_pointer(void *n, int *result);
+void	ft_hexpointer(unsigned long long nb, int *result);
+void	ft_format(char fmt, va_list vl, int *result);
 
 
 #endif

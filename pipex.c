def4ccdb74e096f1d2cd8abeab91928851b28801
Hera.r <tennyhera.r@gmail.com>
2023-03-20 21:03:50 +0100

    gestion erreur en cas absence infile

--- pipex.c
+++ pipex.c
@@ -22,17 +22,32 @@ t_fd	get_file_descriptor(char **argv)
 	fd.second = open(argv[4], O_CREAT | O_WRONLY, 0644);
 	if (fd.second < 0)
 		perror("Erreur d'ouverture du fichier");
-	if (fd.first < 0 && fd.second > 0)
-		close(fd.second);
-	if (fd.second < 0 && fd.first > 0)
-		close(fd.first);
+	// if (fd.first < 0 && fd.second > 0)
+	// 	close(fd.second);
+	// if (fd.second < 0 && fd.first > 0)
+	// 	close(fd.first);
 	return (fd);
+}
+
+int	argv_error(int argc)
+{
+	if (argc > 5)
+	{
+		ft_printf("Erreur : Trop d'arguments.\n");
+		return (1);
+	}
+	else if (argc < 5)
+	{
+		ft_printf("Erreur : Pas assez d'arguments.\n");
+		return (1);
+	}
+	return (0);
 }
 
 int	pipex(char **argv, char **envp, t_fd fd)
 {
 	pid_t		pid;
-	int			status;
+	pid_t		pid2;
 	int			fd_pipe[2];
 
 	test_pipe_and_fd(fd, fd_pipe, argv);
@@ -42,18 +57,31 @@ int	pipex(char **argv, char **envp, t_fd fd)
 	else if (pid == 0)
 	{
 		close(fd_pipe[0]);
-		first_execve(fd, argv, envp, fd_pipe[1]);
-		exit(EXIT_SUCCESS);
+		if (fd.first > 0)
+		{
+			if (first_execve(fd, argv, envp, fd_pipe[1]) != 0)
+				return (close(fd_pipe[1]), close(fd.first), close(fd.second), 1);
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_FAILURE);
 	}
-	else
+    pid2 = fork();
+    if (pid2 == -1)
+	{
+        perror("fork");
+        exit(EXIT_FAILURE);
+	}
+	else if (pid2 == 0)
 	{
 		close(fd_pipe[1]);
-		second_execve(argv, envp, fd_pipe[0], fd);
-		wait(&status);
+		if (second_execve(argv, envp, fd_pipe[0], fd) != 0)
+			return (close(fd_pipe[0]), close(fd.first), close(fd.second), 1);
 	}
-	close(fd.first);
-	close(fd.second);
-	return (-1);
+	close(fd_pipe[0]);
+	close(fd_pipe[1]);
+	waitpid(pid, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	return (close(fd.first), close(fd.second), 1);
 }
 
 int	main(int argc, char *argv[], char *envp[])
@@ -62,23 +90,18 @@ int	main(int argc, char *argv[], char *envp[])
 
 	if (argc == 5)
 	{
+		if (ft_len(argv[2]) == 0 && ft_len(argv[3]) == 0)
+		{
+			ft_printf("Commande \"\" introuvable\nCommande \"\" introuvable\n");
+			fd.second = open(argv[4], O_CREAT | O_WRONLY, 0644);
+			return (close(fd.second), 1);
+		}
 		fd = get_file_descriptor(argv);
-		if (fd.first == -1 || fd.second == -1)
-			return (-1);
 		pipex(argv, envp, fd);
 	}
 	else
 	{
-		if (argc > 5)
-		{
-			ft_printf("Erreur : Trop d'arguments.\n");
-			return (1);
-		}
-		else if (argc < 5)
-		{
-			ft_printf("Erreur : Pas assez d'arguments.\n");
-			return (1);
-		}
+		argv_error(argc);
 	}
 	return (0);
 }

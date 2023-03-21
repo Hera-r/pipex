46c44591cc154479fcdda87fa82bdd4230b2e90d
Hera.r <tennyhera.r@gmail.com>
2023-03-21 13:23:29 +0100

    factorisation code

--- pipex.c
+++ pipex.c
@@ -22,26 +22,7 @@ t_fd	get_file_descriptor(char **argv)
 	fd.second = open(argv[4], O_CREAT | O_WRONLY, 0644);
 	if (fd.second < 0)
 		perror("Erreur d'ouverture du fichier");
-	// if (fd.first < 0 && fd.second > 0)
-	// 	close(fd.second);
-	// if (fd.second < 0 && fd.first > 0)
-	// 	close(fd.first);
 	return (fd);
-}
-
-int	argv_error(int argc)
-{
-	if (argc > 5)
-	{
-		ft_printf("Erreur : Trop d'arguments.\n");
-		return (1);
-	}
-	else if (argc < 5)
-	{
-		ft_printf("Erreur : Pas assez d'arguments.\n");
-		return (1);
-	}
-	return (0);
 }
 
 int	pipex(char **argv, char **envp, t_fd fd)
@@ -55,28 +36,15 @@ int	pipex(char **argv, char **envp, t_fd fd)
 	if (pid == -1)
 		return (perror("Erreur de création du processus fils"), 1);
 	else if (pid == 0)
+		first_cmd(fd_pipe, fd, argv, envp);
+	pid2 = fork();
+	if (pid2 == -1)
 	{
-		close(fd_pipe[0]);
-		if (fd.first > 0)
-		{
-			if (first_execve(fd, argv, envp, fd_pipe[1]) != 0)
-				return (close(fd_pipe[1]), close(fd.first), close(fd.second), 1);
-			exit(EXIT_FAILURE);
-		}
+		perror("fork");
 		exit(EXIT_FAILURE);
 	}
-    pid2 = fork();
-    if (pid2 == -1)
-	{
-        perror("fork");
-        exit(EXIT_FAILURE);
-	}
 	else if (pid2 == 0)
-	{
-		close(fd_pipe[1]);
-		if (second_execve(argv, envp, fd_pipe[0], fd) != 0)
-			return (close(fd_pipe[0]), close(fd.first), close(fd.second), 1);
-	}
+		second_cmd(fd_pipe, fd, argv, envp);
 	close(fd_pipe[0]);
 	close(fd_pipe[1]);
 	waitpid(pid, NULL, 0);
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: hrandria <hrandria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 12:36:16 by hrandria          #+#    #+#             */
-/*   Updated: 2023/02/27 18:06:52 by hrandria         ###   ########.fr       */
+/*   Updated: 2023/03/21 13:15:07 by hrandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void	ftab(char **tab);
 char	**ft_split(char const *s, char c);
 char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_printf(const char *fmt, ...);
+int		argv_error(int argc);
 int		ft_putchar(char c);
 int		ft_putstr(char *s);
 void	ft_putnbr(int nb, int *result);
@@ -48,6 +49,8 @@ typedef struct t_fd{
 }			t_fd;
 t_fd	get_file_descriptor(char **argv);
 size_t	ft_len(const char *s);
+void	first_cmd(int fd_pipe[], t_fd fd, char **argv, char **envp);
+void	second_cmd(int fd_pipe[], t_fd fd, char **argv, char **envp);
 int		pipex(char **argv, char **envp, t_fd fd);
 int		first_execve(t_fd fd, char **argv, char **envp, int fd_pipe1);
 int		second_execve(char **argv, char **envp, int fd_pipe0, t_fd fd);
